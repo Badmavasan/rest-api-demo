3964a561a8135679835d16d27c1cb2e6a9ba80d5
Badmavasan KIROUCHENASSAMY <badmavasan.kirouchenassamy@lip6.fr>
2024-08-06 13:58:44 +0200

    init

---
+++ src/test/java/com/application/res_api_demo/ResApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.res_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
